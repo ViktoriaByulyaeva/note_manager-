dca0684538dc7792907ed0a0092c1a9cba3145b8
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-04 19:35:11 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,17 @@
+username = "Имя пользователя"
+print (username)
+
+title = "Название заметки"
+print (title)
+
+content = "Содержание"
+print (content)
+
+status = "Статус"
+print (status)
+
+created_date = "Дата создания"
+print (created_date)
+
+issue_date = "Дедлайн"
+print (issue_date)
