f4354d8c81c1bcab451f313c403811dc5415b979
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-07 23:36:47 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,6 @@
+title1=input ("Введите название заголовка №1 ")
+title2=input ("Ведите название заголовка №2 ")
+title3=input ("Введите название заголовка №3 ")
+
+titles=[title1, title2, title3]
+print(titles)
