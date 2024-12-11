6728cf04059ca08ded3c1518aaacb813b52130fa
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-11 17:14:13 +0300

    Update final.py
    .

--- final.py
+++ final.py
@@ -1,10 +1,30 @@
-note=[
-    "Имя пользователя",
-    "Название заметки,"
-    "Содержание",
-    "Статус",
-    "Дата создания",
-    "Дедлайн",
-    ["Заголовок №1", "Заголовок №2", "Заголовок №3"
-     ] #вложенный список
-]
+username = "Имя пользователя"
+print (username)
+title = "Название заметки"
+print (title)
+content = "Содержание"
+print (content)
+status = "Статус"
+print (status)
+created_date = "Дата создания"
+print (created_date)
+issue_date = "Дедлайн"
+print (issue_date)
+
+title=input ("Введите название заметки")
+title1=input ("Заголовок №1 ")
+title2=input ("Заголовок №2 ")
+title3=input ("Заголовок №3 ")
+
+titles=[title, title1, title2, title3]
+print(titles)
+
+note={
+    username,
+    title,
+    content,
+    status,
+    created_date,
+    issue_date,
+    [title1, title2, title3]
+}
