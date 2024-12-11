07f17119a390d27f5e4a43124ccb4c4c80898091
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-11 14:58:06 +0300

    Update add_list.py
    .

--- add_list.py
+++ add_list.py
@@ -1,6 +1,7 @@
-title1=input ("Введите название заголовка №1 ")
-title2=input ("Ведите название заголовка №2 ")
-title3=input ("Введите название заголовка №3 ")
+title=input ("Введите название заметки")
+title1=input ("Заголовок №1 ")
+title2=input ("Заголовок №2 ")
+title3=input ("Заголовок №3 ")
 
-titles=[title1, title2, title3]
+titles=[title, title1, title2, title3]
 print(titles)
