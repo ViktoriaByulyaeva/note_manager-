b192a8c2916b996cc9b11f4c0f6e19e23f9099a2
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-17 21:31:08 +0300

    Update date_changer.py
    .

--- date_changer.py
+++ date_changer.py
@@ -1,20 +1,15 @@
+import datetime
+
 created_date="Дата создания"
 print(created_date)
-
-import datetime
 created_date=(datetime.datetime.now())
 print(created_date)
-
-from datetime import datetime
-temp_created_date=(datetime.now().strftime("%d-%m"))
+temp_created_date=(datetime.datetime.now().strftime("%d-%m"))
 print(temp_created_date)
 
 issue_date="Дедлайн"
 print(issue_date)
-import datetime
 issue_date=(datetime.datetime.now())
 print(issue_date)
-
-from datetime import datetime
-temp_issue_date=(datetime.now().strftime("%d-%m"))
+temp_issue_date=(datetime.datetime.now().strftime("%d-%m"))
 print(temp_issue_date)
