d8ca460bdf0c5dbc01693aeec8bfe1344d928510
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-17 23:41:52 +0300

    Update final.py
    исправлены ошибки

--- final.py
+++ final.py
@@ -1,30 +1,17 @@
-username = "Имя пользователя"
-print (username)
-title = "Название заметки"
-print (title)
-content = "Содержание"
-print (content)
-status = "Статус"
-print (status)
-created_date = "Дата создания"
-print (created_date)
-issue_date = "Дедлайн"
-print (issue_date)
-
-title=input ("Введите название заметки")
-title1=input ("Заголовок №1 ")
-title2=input ("Заголовок №2 ")
-title3=input ("Заголовок №3 ")
-
-titles=[title, title1, title2, title3]
-print(titles)
-
-note={
-    username,
-    title,
-    content,
-    status,
-    created_date,
-    issue_date,
-    [title1, title2, title3]
+note = {
+    'username' : input ('Введите имя пользователя '),
+    'title' : input ('Введите название заметки '),
+    'content' : input ('Введите содержание заметки '),
+    'status' : input ('Введите статус заметки '),
+    'created_date' : input('Дата создания: '),
+    'issue_date' : input('Дедлайн: '),
+    'titles' : []
 }
+
+title1 = input('Заголовок №1 ')
+title2 = input('Заголовок №2 ')
+title3 = input('Заголовок №3 ')
+titles = [title1, title2, title3]
+note ['titles'].append(titles)
+
+print(note)
