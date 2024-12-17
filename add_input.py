3506947f6ff3ebd202fa1888c8f447820bc29e28
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-17 20:23:45 +0300

    Update add_input.py
    исправлена ошибка импорта

--- add_input.py
+++ add_input.py
@@ -1,9 +1,10 @@
+import datetime
 username = input ("Как Вас зовут? ")
 print ("Здравствуйте, ", username, "!")
 title = input ("введите название заметки ")
 content = input ("Расскажите о своих планах и целях, " + username + " ")
 status = input( "Введите статус заметки ")
-import datetime
+
 created_date=(datetime.datetime.now())
 print("Дата создания заметки: ", created_date.strftime("%d-%m"))
 issue_date = input ("Дата, когда нужно закончить ")
