ea59bca97672211d362265b6ff14b2b1c27a9495
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-22 01:02:28 +0300

    Update check_deadline.py
    .

--- check_deadline.py
+++ check_deadline.py
@@ -1,9 +1,13 @@
 import datetime
-
 create_date = datetime.datetime.now() #дата сейчас (момент создания)
 print ('Дата создания: ', datetime.datetime.now().strftime('%d-%m-%Y'))
 
-issue_date = datetime.datetime.strptime(input ('Дедлайн: (введите дату в формате "день-месяц-год"): '), '%d-%m-%Y') #дедлайн от пользователя
+while True:
+    try: #формат даты
+        issue_date = datetime.datetime.strptime(input('Дедлайн: (введите дату в формате "день-месяц-год"): '), '%d-%m-%Y')  # дедлайн от пользователя
+        break
+    except ValueError:
+        print('Некорректная дата')
 
 #условия:
 if create_date.day == issue_date.day and create_date.month == issue_date.month and create_date.year == issue_date.year:
@@ -14,5 +18,5 @@ elif create_date > issue_date :
 
 else:
     period = issue_date - create_date
-    print ('Осталось {} дней'.format(period.days))
+print ('Осталось {} дней'.format(period.days))
 
