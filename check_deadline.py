d6696f223a7af1eaa138553425eaccf54975a009
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-21 19:57:30 +0300

    Add files via upload

---
+++ check_deadline.py
@@ -0,0 +1,18 @@
+import datetime
+
+create_date = datetime.datetime.now() #дата сейчас (момент создания)
+print ('Дата создания: ', datetime.datetime.now().strftime('%d-%m-%Y'))
+
+issue_date = datetime.datetime.strptime(input ('Дедлайн: (введите дату в формате "день-месяц-год"): '), '%d-%m-%Y') #дедлайн от пользователя
+
+#условия:
+if create_date.day == issue_date.day and create_date.month == issue_date.month and create_date.year == issue_date.year:
+    print("Срок выполнения сегодня")
+
+elif create_date > issue_date :
+    print('Срок выполнения задачи прошел')
+
+else:
+    period = issue_date - create_date
+    print ('Осталось {} дней'.format(period.days))
+
