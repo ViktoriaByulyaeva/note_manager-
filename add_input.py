2f6309a5c1832dcc288e10d8f528a5861e3d8b73
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-21 18:57:13 +0300

    Update add_input.py
    .

--- add_input.py
+++ add_input.py
@@ -1,11 +1,15 @@
-import datetime
-username = input ("Как Вас зовут? ")
+import datetime # импорт даты
+username = input ("Как Вас зовут? Введите Ваше имя: ")
 print ("Здравствуйте, ", username, "!")
-title = input ("введите название заметки ")
-content = input ("Расскажите о своих планах и целях, " + username + " ")
-status = input( "Введите статус заметки ")
+title = input ("Введите название заметки: ")
+content = input ("Расскажите о своих планах и целях, " + username + ": ")
+status = input( "Введите статус заметки: ")
 
-created_date=(datetime.datetime.now())
-print("Дата создания заметки: ", created_date.strftime("%d-%m"))
-issue_date = input ("Дата, когда нужно закончить ")
-print(username, "," , issue_date, "подойдет дедлайн", title)
+created_date = (datetime.datetime.now()) #дата создания (сейчас)
+print ("Дата создания заметки: ", created_date.strftime("%d-%m")) #Сокращение даты до д-м
+
+issue_date="Дедлайн: "
+print(issue_date)
+issue_date = datetime.datetime.strptime(input ('Введите дату в формате "день-месяц-год": '), '%d-%m-%Y') #пользователь вводит дату
+temp_issue_date = (issue_date.strftime('%d-%m')) #сокращаем дату
+print (username, "," , temp_issue_date, "подойдет дедлайн", title) #итог
