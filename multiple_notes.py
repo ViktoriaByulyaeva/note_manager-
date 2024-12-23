e823c37fd01fb375578fd6b6e9a4fc685247b015
Виктория <viktoriabyulyaeva@yandex.com>
2024-12-24 00:18:58 +0300

    Update multiple_notes.py
    улучшено

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,3 +1,4 @@
+import datetime
 notes = [] #список
 
 print ("Привет. Я - планировщик задач. ") #приветствие
@@ -13,11 +14,42 @@ while True: #бесконечный цикл
     print('1) Готово')  # варианты ответов:
     print("2) В процессе")
     print("3) Отложено")
-    status = input("Ваш ответ: ")  # ввести ответ
-    created_date = input('Дата создания заметки: ')
-    issue_date = input('Дедлайн: ')
+    while True:
+        status = input("Ваш ответ: ")  # ввести ответ33
+        if status.lower() in ['1', 'готово']:
+            print("Статус заметки обновлен на: Готово")
+            break
+        elif status.lower() in ['2', 'в процессе']:
+            print('Статус заметки обновлен на: В процессе')
+            break
+        elif status.lower() in ['3', 'отложено']:
+            print("Статус заметки обновлен на: Отложено")
+            break
+        else:  # если пользователь ввел иные значения в ответ
+            print("Некорректный ответ. Повторите попытку и выберете из предложенных вариантов")
+    create_date = 'Дата создания'
+    create_date = datetime.datetime.now()  # дата сейчас (момент создания)
+    print('Дата создания: ', datetime.datetime.now().strftime('%d-%m-%Y'))
+    while True:
+        try:  # формат даты
+            issue_date = datetime.datetime.strptime(input('Дедлайн: (введите дату в формате "день-месяц-год"): '),
+                                                    '%d-%m-%Y')  # дедлайн от пользователя
+            break
+        except ValueError:
+            print('Некорректная дата')
 
-    note1 = {'Имя пользователя' : username, 'Название заметки' : title, 'Содержание' : content, 'Статус' : status, 'Дата создания' : created_date, 'Дедлайн' : issue_date} #словарь
+    # условия:
+    if create_date.day == issue_date.day and create_date.month == issue_date.month and create_date.year == issue_date.year:
+        print("Срок выполнения сегодня")
+
+    elif create_date > issue_date:
+        print('Срок выполнения задачи прошел')
+
+    else:
+        period = issue_date - create_date
+    print('Осталось {} дней'.format(period.days))
+
+    note1 = {'Имя пользователя' : username, 'Название заметки' : title, 'Содержание' : content, 'Статус' : status, 'Дата создания' : create_date, 'Дедлайн' : issue_date} #словарь
     notes.append(note1) #добавить словарь в список
 
     note2 = input('Нужна еще одна заметка? Впишите ответ: да/нет. Ответ: ')
